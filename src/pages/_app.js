4df830748eb4728b02a453dc20f94f7240e466fb
ranokhassanova <khasanova.rano@gmail.com>
2023-04-27 11:26:02 +0600

    blog

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/globals.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/_styles.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
